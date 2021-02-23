3edf1e2dd0461bce8ba5cd192c4917cb4b1746b2
gsadanala <gsadanala@paypal.com>
2021-02-23 15:05:36 +0530

    Create JokeController

---
+++ src/main/java/com/ganesh/springbootjokesapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.ganesh.springbootjokesapp.controllers;
+
+import com.ganesh.springbootjokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+//    returns view name
+//    Model is a map implementation
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
